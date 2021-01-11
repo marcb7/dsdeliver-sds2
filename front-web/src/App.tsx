f449d7df5306917bf488658c756f1fbe2d9cde1f
unknown <b3darcademy@outlook.com>
2021-01-11 04:21:56 -0300

    Create front-web initial files

---
+++ front-web/src/App.tsx
@@ -0,0 +1,12 @@
+// import React from 'react'; => Nas novas versões do React App, esse import do React não é mais necessário.
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello!</h1>
+    </div>
+  );
+}
+
+export default App;
