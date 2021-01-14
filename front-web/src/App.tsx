10948d77787fa9f0525856659fcccf57bed56b67
unknown <b3darcademy@outlook.com>
2021-01-14 02:00:59 -0300

    Add Navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,11 @@
 // import React from 'react'; => Nas novas versões do React App, esse import do React não é mais necessário.
 import './App.css';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div>
-      <h1>Hello!</h1>
+      <Navbar/>
     </div>
   );
 }
